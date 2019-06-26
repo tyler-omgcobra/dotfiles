a01e95ac7bcca99ccd968fdf992c2c6726d2f901
Matt Snyder <matt@cranialpulse.com>
2019-06-26 07:58:32 -0400

    Remove the vim stuff. Move to bare git essentials

--- .oh-my-zsh-custom/ssh.zsh
+++
@@ -1,4 +0,0 @@
-h=(${${(s: :)${(ps:\t:)${${(@M)${(f)"$(<$HOME/.ssh/config)"}:#Host *}#Host}}}:#*[*?]*})
-
-zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):*' hosts $h
-zstyle ':completion:*:(ssh|scp|sftp|rsh|rsync):*' ignored-patterns 'ec2*.compute.amazonaws.com' 'heroku.*'
