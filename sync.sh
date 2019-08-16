d0e5d6b7f44e0c6728c093a827be80349a0f9c32
Tyler Smith <tyler@omgcobra.org>
2019-08-15 21:15:36 -0400

    Hide completion and prompt scripts

--- sync.sh
+++ sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 dir=~/dotfiles                    # dotfiles directory
-files=".gitignore_global .gitconfig .githelpers git_completion.bash git_prompt.sh"    # list of files/folders to symlink in homedir
+files=".gitignore_global .gitconfig .githelpers .git-completion.bash .git-prompt.sh"    # list of files/folders to symlink in homedir
 
 echo "Changing to the $dir directory"
 cd $dir
